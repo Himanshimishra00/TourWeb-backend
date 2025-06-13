19b61db4f587450a107be1d59f9745d86310a4cb
himanshi <himumishrao2@gmail.com>
2025-06-13 22:09:31 +0530

    final commit

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 db()
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 
 app.use('/api/destinations', destinationRoutes);
